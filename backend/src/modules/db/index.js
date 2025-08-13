afc3e8039c7954c4ae7fc8dae79d4e79bc23c686
laraproto <laraproto@proton.me>
2025-08-13 17:58:03 +0300

    leftover tsc garbage

--- backend/src/modules/db/index.js
+++
@@ -1,5 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import * as schema from "./schema";
-import { DATABASE_URL } from "@modules/config";
-export const db = drizzle(DATABASE_URL, { schema });
-export * from "./schema";
