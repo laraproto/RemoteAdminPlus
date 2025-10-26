4bec24d5809ab5e7745b93eb37bd79f9b2aaf27d
Lara <laraproto@proton.me>
2025-10-26 18:13:16 +0200

    define routes

--- backend/src/routes/trpc/authed/index.ts
+++ backend/src/routes/trpc/authed/index.ts
@@ -1,9 +1,13 @@
 import { router } from "#modules/trpc/index";
 import userRouter from "./user";
+import bansRouter from "./bans";
+import warnsRouter from "./warns";
 //import { z } from "zod";
 
 const authedRouter = router({
   user: userRouter,
+  warns: warnsRouter,
+  bans: bansRouter,
 });
 
 export default authedRouter;
